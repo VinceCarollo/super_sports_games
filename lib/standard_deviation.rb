2e7bfe2dba54e30db7a4aee9c0c1cda890287353
Vince Carollo <vincecarollo@gmail.com>
2019-03-28 12:39:45 -0600

    Add space for ssh key change test

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -5,4 +5,5 @@ avg = ages.sum / ages.length.to_f
 subtract_each = ages.map{|num| (num - avg).round(2) }
 square_each = subtract_each.map{|num| (num * num).round(2) }
 standard_deviation =  (Math.sqrt(square_each.sum / ages.length)).round(2)
+
 p standard_deviation
